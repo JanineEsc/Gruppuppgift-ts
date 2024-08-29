8316b54a4d4331d03d17824202e82bc0c1d5ca10
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-08-29 10:30:03 +0200

    adds types

--- internet-forum/app/types/post.d.ts
+++ internet-forum/app/types/post.d.ts
@@ -1,3 +1,28 @@
-type Post = {
-    
+type ThreadCategory = "Thread" | "QNA"
+
+type User = {
+  userName: string,
+  password: string,  
+}
+
+type Thread = {
+  id: number;
+  title: string;
+  category: ThreadCategory;
+  creationDate: string;
+  description: string;
+  creator: User;
+}
+
+type QNAThread = Thread & {
+  category: "QNA";
+  isAnswered: boolean;
+  commentAnswerId?: number;
+}
+
+type Comment = {
+  id: number;
+  thread: number;
+  content: string;
+  creator: User;
 }
