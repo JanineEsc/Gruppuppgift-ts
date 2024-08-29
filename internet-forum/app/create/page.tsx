a71d0dc9929719af73b3cfd6813ffc681d495df8
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-08-29 17:01:30 +0200

    added text area on createThread page

--- internet-forum/app/create/page.tsx
+++ internet-forum/app/create/page.tsx
@@ -1,7 +1,8 @@
+import CreateThread from "@/components/ui/CreateThread";
 
 export default function Create() {
     return (
-       <h1 className="font-bold text-center text-6xl">Create</h1>
+       <CreateThread/>
     );
   }
   
