ca487b2bda21a80ca1d0053fbb0f494eb06768f4
Peter <peterblomkvist@hotmail.com>
2024-08-29 16:38:44 +0200

    added thread component to thread

--- internet-forum/app/threads/page.tsx
+++ internet-forum/app/threads/page.tsx
@@ -1,7 +1,19 @@
+import ThreadComponent from "@/components/ThreadComponent";
+import Link from "next/link";
 
 export default function Threads() {
     return (
-       <h1 className="font-bold text-center text-6xl">Threads</h1>
+        <main>
+            {/* Navbar */}
+            <h1 className="font-bold text-center text-6xl mb-4">Threads</h1>
+            <div className="flex items-center gap-5 mb-4">
+                <Link href={"/"} className="text-blue-500 hover:underline">Forum</Link>
+                <p className="text-gray-700">Title</p>   
+            </div>
+            <ThreadComponent/>
+            <ThreadComponent/>
+            <ThreadComponent/>
+            <ThreadComponent/>
+        </main>
     );
-  }
-  
+}
