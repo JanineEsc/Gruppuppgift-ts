13768c8ece048382e7d1163f9ba7f0995401c6ee
Peter <peterblomkvist@hotmail.com>
2024-08-30 20:30:38 +0200

    added thread category

--- internet-forum/app/threads/[id]/page.tsx
+++ internet-forum/app/threads/[id]/page.tsx
@@ -1,52 +1,47 @@
-"use client";
+// "use client";
 
-import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+// import { useParams } from 'next/navigation';
+// import { useEffect, useState } from 'react';
 
-type Thread = {
-  id: string;
-  title: string;
-  content: string;
-  timestamp: string;
-};
 
-const ThreadDetail = () => {
-  const { id } = useParams(); 
 
-  const [thread, setThread] = useState<Thread | null>(null);
+// const ThreadDetail = () => {
+//   const { id } = useParams(); 
 
-  useEffect(() => {
-    console.log("Thread ID from URL:", id); 
+//   const [thread, setThread] = useState<Thread | null>(null);
 
-    if (id) {
-      const savedThreads = localStorage.getItem('threads');
-      if (savedThreads) {
-        const threads: Thread[] = JSON.parse(savedThreads);
-        const foundThread = threads.find(thread => thread.id === id);
-        setThread(foundThread || null);
-      }
-    }
-  }, [id]);
+//   useEffect(() => {
+//     console.log("Thread ID from URL:", id); 
 
-  if (!thread) {
-    return <p className="text-red-500 text-center text-8xl">Thread not found.</p>;
-  }
+//     if (id) {
+//       const savedThreads = localStorage.getItem('threads');
+//       if (savedThreads) {
+//         const threads: Thread[] = JSON.parse(savedThreads);
+//         const foundThread = threads.find(thread => thread.id === id);
+//         setThread(foundThread || null);
+//       }
+//     }
+//   }, [id]);
 
-  return (
-    <main className="border-2 flex items-center justify-center p-10">
-      <div className="w-full h-full grid-cols-1 bg-blue md:grid-cols-2">
-        <div className="text-white flex items-center justify-center flex-col">
-          <div className="my-4">
-            <div className="max-w-lg mx-auto mt-10 p-6 rounded shadow-md">
-              <h1 className="text-2xl font-bold mb-4">{thread.title}</h1>
-              <p className="text-sm text-slate-400">{new Date(thread.timestamp).toLocaleString()}</p>
-              <p className="mt-2 text-sm">{thread.content}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-};
+//   if (!thread) {
+//     return <p className="text-red-500 text-center text-8xl">Thread not found.</p>;
+//   }
 
-export default ThreadDetail;
+//   return (
+//     <main className="border-2 flex items-center justify-center p-10">
+//       <div className="w-full h-full grid-cols-1 bg-blue md:grid-cols-2">
+//         <div className="text-white flex items-center justify-center flex-col">
+//           <div className="my-4">
+//             <div className="max-w-lg mx-auto mt-10 p-6 rounded shadow-md">
+//               <h1 className="text-2xl font-bold mb-4">{thread.title}</h1>
+//               <p className="text-sm text-slate-400">{new Date(thread.timestamp).toLocaleString()}</p>
+//               <p className="mt-2 text-sm">{thread.content}</p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// };
+
+// export default ThreadDetail;
--- internet-forum/components/ListThreads.tsx
+++ internet-forum/components/ListThreads.tsx
@@ -2,12 +2,12 @@
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
-type Thread = {
-  id: string;
-  title: string;
-  content: string;
-  timestamp: string;
-};
+// type Thread = {
+//   id: string;
+//   title: string;
+//   content: string;
+//   timestamp: string;
+// };
 
 function ListThreads() {
   const [threads, setThreads] = useState<Thread[]>([]);
@@ -22,7 +22,7 @@ function ListThreads() {
     }
   }, []);
 
-  const deleteThread = (id: string) => {
+  const deleteThread = (id: number) => {
     const updatedThreads = threads.filter(thread => thread.id !== id);
     setThreads(updatedThreads);
     localStorage.setItem('threads', JSON.stringify(updatedThreads));
@@ -40,8 +40,9 @@ function ListThreads() {
                 threads.map(thread => (
                   <div key={thread.id} className="flex justify-between items-center mb-4 p-4 border border-white w-[800px]">
                     <Link href={`/threads/${thread.id}`}>{thread.title}</Link>
-                    <p className="text-sm text-slate-400">{new Date(thread.timestamp).toLocaleString()}</p>
-                    <p className="mt-2 text-sm">{thread.content}</p>
+                    <p className="text-sm text-slate-400">{new Date(thread.creationDate).toLocaleString()}</p>
+                    <p className="mt-2 text-sm">{thread.category}</p>
+                    {/* <p className="mt-2 text-sm">{thread.description}</p> */}
                     <button
                       className="mt-2 bg-red-500 text-white px-4  "
                       onClick={() => deleteThread(thread.id)}
